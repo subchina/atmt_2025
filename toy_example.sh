7eb693107d67bd2276895543b5328d85086d6de8
subchina <andela.matic@uzh.com>
2025-10-10 18:56:42 +0200

    Adding files

--- toy_example.sh
+++ toy_example.sh
@@ -7,7 +7,7 @@
 #SBATCH --gpus=1
 #SBATCH --output=toy_example.out
 
-# module load gpu
+module load gpu
 module load mamba
 source activate atmt
 export XLA_FLAGS=--xla_gpu_cuda_data_dir=$CONDA_PREFIX/pkgs/cuda-toolkit
@@ -53,7 +53,8 @@ python train.py \
     --dim-feedforward-decoder 1024 \
     --max-seq-len 100 \
     --n-encoder-layers 3 \
-    --n-decoder-layers 3
+    --n-decoder-layers 3 \
+    --cuda
 
 python translate.py \
     --input toy_example/data/raw/test.cz \
