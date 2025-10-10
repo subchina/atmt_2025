2ff9418b686454bd3fb754d5a174cf3dbb661d96
subchina <131053435+subchina@users.noreply.github.com>
2025-10-10 19:20:14 +0200

    Update task3.sh

--- task3.sh
+++ task3.sh
@@ -12,30 +12,6 @@ module load mamba
 source activate atmt
 export XLA_FLAGS=--xla_gpu_cuda_data_dir=$CONDA_PREFIX/pkgs/cuda-toolkit
 
-# TRAIN
-python train.py \
-    --cuda \
-    --data cz-en/data/prepared/ \
-    --src-tokenizer cz-en/tokenizers/cz-bpe-8000.model \
-    --tgt-tokenizer cz-en/tokenizers/en-bpe-8000.model \
-    --source-lang cz \
-    --target-lang en \
-    --batch-size 64 \
-    --arch transformer \
-    --max-epoch 7 \
-    --log-file cz-en/logs/train.log \
-    --save-dir cz-en/checkpoints/ \
-    --ignore-checkpoints \
-    --encoder-dropout 0.1 \
-    --decoder-dropout 0.1 \
-    --dim-embedding 256 \
-    --attention-heads 4 \
-    --dim-feedforward-encoder 1024 \
-    --dim-feedforward-decoder 1024 \
-    --max-seq-len 300 \
-    --n-encoder-layers 3 \
-    --n-decoder-layers 3 
-
 # TRANSLATE
 python translate.py \
     --cuda \
@@ -43,5 +19,5 @@ python translate.py \
     --src-tokenizer cz-en/tokenizers/cz-bpe-8000.model \
     --tgt-tokenizer cz-en/tokenizers/en-bpe-8000.model \
     --checkpoint-path cz-en/checkpoints/checkpoint_best.pt \
-    --output cz-en/output.txt \
+    --output cz-en/output/output_task3.txt \
     --max-len 300
