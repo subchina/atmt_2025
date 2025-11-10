ba2b5016d1827666414332d4bddd7cb0483a4f22
subchina <131053435+subchina@users.noreply.github.com>
2025-11-11 00:31:13 +0100

    Uncomment module loading and environment setup

--- loadfromcheckpoint.sh
+++ loadfromcheckpoint.sh
@@ -7,10 +7,10 @@
 #SBATCH --gpus=1
 #SBATCH --output=toy_example.out
 
-# module load gpu
-# module load mamba
-# source activate atmt
-# export XLA_FLAGS=--xla_gpu_cuda_data_dir=$CONDA_PREFIX/pkgs/cuda-toolkit
+module load gpu
+module load mamba
+source activate atmt
+export XLA_FLAGS=--xla_gpu_cuda_data_dir=$CONDA_PREFIX/pkgs/cuda-toolkit
 
 # clean up from previous runs
 # rm -rf toy_example/data/prepared
