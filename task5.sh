4aa988ac2be778c9a747bfb78dff6674482e431e
subchina <andjelam0810@gmail.com>
2025-12-09 20:39:57 +0100

    Make ex05 file

---
+++ task5.sh
@@ -0,0 +1,25 @@
+#!/usr/bin/bash -l
+#SBATCH --partition teaching
+#SBATCH --time=24:0:0
+#SBATCH --ntasks=1
+#SBATCH --mem=16GB
+#SBATCH --cpus-per-task=1
+#SBATCH --gpus=1
+#SBATCH --output=out_task3.out
+
+module load gpu
+module load miniforge3
+source activate atmt
+export XLA_FLAGS=--xla_gpu_cuda_data_dir=$CONDA_PREFIX/pkgs/cuda-toolkit
+
+# TRANSLATE
+python seq2seq\ decode.py \
+    --cuda \
+    --input cz-en/data/prepared/test.sk \
+    --src-tokenizer cz-en/tokenizers/cz-bpe-8000.model \
+    --tgt-tokenizer cz-en/tokenizers/en-bpe-8000.model \
+    --checkpoint-path cz-en/checkpoints/checkpoint_best.pt \
+    --max-len 300 \
+    --output cz-en/output/output_ex05.txt \
+    --bleu \
+    --reference sk-en/data/prepared/test.en
